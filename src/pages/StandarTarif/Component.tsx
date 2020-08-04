8b707554af65b87a0582036fd4a29c87e6e285cc
Arief Yusron <ariefyusron98@gmail.com>
2020-08-04 23:33:54 +0700

    filter standar tarif

--- src/pages/StandarTarif/Component.tsx
+++ src/pages/StandarTarif/Component.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import { Reducers } from "../../redux/types";
@@ -16,6 +16,7 @@ const Component = () => {
   documentTitle("Standar Tarif");
   const dispatch = useDispatch();
 
+  const [selectPekerjaan, setSelectPekerjaan] = useState("");
   const [modalAddPekerjaan, setModalAddPekerjaan] = useState(false);
   const [modalEditPekerjaan, setModalEditPekerjaan] = useState({
     visible: false,
@@ -37,6 +38,127 @@ const Component = () => {
     dispatch(getStandarTarif());
   }, [dispatch]);
 
+  const _renderTable = useCallback(
+    (e: any, i: number) => (
+      <Row style={{ marginBottom: 30 }} key={i}>
+        <Col>
+          <Row style={{ marginBottom: 10 }}>
+            <Col size={10}>
+              <h3>{e.name}</h3>
+            </Col>
+            {localStorage.getItem("is_admin") === "true" && (
+              <Col size={2}>
+                <Row justifyContent="end">
+                  <Col size={3}>
+                    <button
+                      type="button"
+                      className="btn btn-success"
+                      onClick={() =>
+                        setModalEditPekerjaan({
+                          visible: true,
+                          index: i,
+                          id: e.id
+                        })
+                      }
+                    >
+                      edit
+                    </button>
+                  </Col>
+                  <Col size={6}>
+                    <button
+                      type="button"
+                      className="btn btn-danger"
+                      style={{ marginLeft: 15 }}
+                      onClick={() =>
+                        dispatch(deletePekerjaanStandarTarif(e.id))
+                      }
+                    >
+                      delete
+                    </button>
+                  </Col>
+                </Row>
+              </Col>
+            )}
+          </Row>
+          <Row>
+            <Col>
+              <table className="table">
+                <thead className="thead-light">
+                  <tr>
+                    <th scope="col">No</th>
+                    <th scope="col">Item Pekerjaan</th>
+                    <th scope="col">Jam Orang</th>
+                    <th scope="col">DPS</th>
+                    <th scope="col">SUB KONT</th>
+                    <th scope="col">Peralatan</th>
+                    <th scope="col">Material</th>
+                    <th scope="col">Material Bantu</th>
+                    <th scope="col">Overhead</th>
+                    <th scope="col">Total HPP</th>
+                    <th scope="col">Standar Tarif Reparasi Kapal</th>
+                    {localStorage.getItem("is_admin") === "true" && (
+                      <th scope="col">Aksi</th>
+                    )}
+                  </tr>
+                </thead>
+                <tbody>
+                  {standarTarifState.list
+                    .filter(
+                      resFilter =>
+                        resFilter.id_pekerjaan.toString() === e.id.toString()
+                    )
+                    .map((item, index) => (
+                      <tr key={index} style={{ textAlign: "center" }}>
+                        <th scope="row">{`${index + 1}.`}</th>
+                        <td>{item.item_pekerjaan}</td>
+                        <td>{item.jam_orang}</td>
+                        <td>{maskedMoney(item.dps)}</td>
+                        <td>{maskedMoney(item.sub_kont)}</td>
+                        <td>{maskedMoney(item.peralatan)}</td>
+                        <td>{maskedMoney(item.material)}</td>
+                        <td>{maskedMoney(item.material_bantu)}</td>
+                        <td>{maskedMoney(item.overhead)}</td>
+                        <td>{maskedMoney(item.total_hpp)}</td>
+                        <td>{maskedMoney(item.standar_tarif)}</td>
+                        {localStorage.getItem("is_admin") === "true" && (
+                          <td>
+                            <button
+                              type="button"
+                              className="btn btn-success"
+                              onClick={() =>
+                                setModalEdit({
+                                  visible: true,
+                                  index,
+                                  id: item.id
+                                })
+                              }
+                            >
+                              edit
+                            </button>
+                            <button
+                              type="button"
+                              className="btn btn-danger"
+                              style={{ marginLeft: 15 }}
+                              onClick={() =>
+                                dispatch(deleteStandarTarif(item.id))
+                              }
+                            >
+                              delete
+                            </button>
+                          </td>
+                        )}
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    ),
+    [dispatch, standarTarifState.list]
+  );
+
   return (
     <Container isLoading={standarTarifState.isLoading}>
       <Row style={{ marginBottom: 10, marginTop: 10 }}>
@@ -45,9 +167,28 @@ const Component = () => {
         </Col>
       </Row>
 
-      {localStorage.getItem("is_admin") === "true" &&
-        standarTarifState.listPekerjaan.length > 0 && (
-          <Row justifyContent="end" style={{ marginBottom: 20 }}>
+      {standarTarifState.listPekerjaan.length > 0 && (
+        <Row className="align-items-center" style={{ marginBottom: 20 }}>
+          <Col size={10}>
+            <Row>
+              <Col size={6}>
+                <select
+                  className="form-control"
+                  defaultValue={selectPekerjaan}
+                  onChange={e => setSelectPekerjaan(e.target.value)}
+                >
+                  <option value="">-- pilih --</option>
+                  {standarTarifState.listPekerjaan.map((item, index) => (
+                    <option key={index} value={item.id}>
+                      {item.name}
+                    </option>
+                  ))}
+                </select>
+              </Col>
+            </Row>
+          </Col>
+
+          {localStorage.getItem("is_admin") === "true" && (
             <Col size={2}>
               <button
                 type="button"
@@ -58,126 +199,17 @@ const Component = () => {
                 Add sub pekerjaan
               </button>
             </Col>
-          </Row>
-        )}
-
-      {standarTarifState.listPekerjaan.map((e, i) => (
-        <Row style={{ marginBottom: 30 }} key={i}>
-          <Col>
-            <Row style={{ marginBottom: 10 }}>
-              <Col size={10}>
-                <h3>{e.name}</h3>
-              </Col>
-              {localStorage.getItem("is_admin") === "true" && (
-                <Col size={2}>
-                  <Row justifyContent="end">
-                    <Col size={3}>
-                      <button
-                        type="button"
-                        className="btn btn-success"
-                        onClick={() =>
-                          setModalEditPekerjaan({
-                            visible: true,
-                            index: i,
-                            id: e.id
-                          })
-                        }
-                      >
-                        edit
-                      </button>
-                    </Col>
-                    <Col size={6}>
-                      <button
-                        type="button"
-                        className="btn btn-danger"
-                        style={{ marginLeft: 15 }}
-                        onClick={() =>
-                          dispatch(deletePekerjaanStandarTarif(e.id))
-                        }
-                      >
-                        delete
-                      </button>
-                    </Col>
-                  </Row>
-                </Col>
-              )}
-            </Row>
-            <Row>
-              <Col>
-                <table className="table">
-                  <thead className="thead-light">
-                    <tr>
-                      <th scope="col">No</th>
-                      <th scope="col">Item Pekerjaan</th>
-                      <th scope="col">Jam Orang</th>
-                      <th scope="col">DPS</th>
-                      <th scope="col">SUB KONT</th>
-                      <th scope="col">Peralatan</th>
-                      <th scope="col">Material</th>
-                      <th scope="col">Material Bantu</th>
-                      <th scope="col">Overhead</th>
-                      <th scope="col">Total HPP</th>
-                      <th scope="col">Standar Tarif Reparasi Kapal</th>
-                      {localStorage.getItem("is_admin") === "true" && (
-                        <th scope="col">Aksi</th>
-                      )}
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {standarTarifState.list
-                      .filter(
-                        resFilter =>
-                          resFilter.id_pekerjaan.toString() === e.id.toString()
-                      )
-                      .map((item, index) => (
-                        <tr key={index} style={{ textAlign: "center" }}>
-                          <th scope="row">{`${index + 1}.`}</th>
-                          <td>{item.item_pekerjaan}</td>
-                          <td>{item.jam_orang}</td>
-                          <td>{maskedMoney(item.dps)}</td>
-                          <td>{maskedMoney(item.sub_kont)}</td>
-                          <td>{maskedMoney(item.peralatan)}</td>
-                          <td>{maskedMoney(item.material)}</td>
-                          <td>{maskedMoney(item.material_bantu)}</td>
-                          <td>{maskedMoney(item.overhead)}</td>
-                          <td>{maskedMoney(item.total_hpp)}</td>
-                          <td>{maskedMoney(item.standar_tarif)}</td>
-                          {localStorage.getItem("is_admin") === "true" && (
-                            <td>
-                              <button
-                                type="button"
-                                className="btn btn-success"
-                                onClick={() =>
-                                  setModalEdit({
-                                    visible: true,
-                                    index,
-                                    id: item.id
-                                  })
-                                }
-                              >
-                                edit
-                              </button>
-                              <button
-                                type="button"
-                                className="btn btn-danger"
-                                style={{ marginLeft: 15 }}
-                                onClick={() =>
-                                  dispatch(deleteStandarTarif(item.id))
-                                }
-                              >
-                                delete
-                              </button>
-                            </td>
-                          )}
-                        </tr>
-                      ))}
-                  </tbody>
-                </table>
-              </Col>
-            </Row>
-          </Col>
+          )}
         </Row>
-      ))}
+      )}
+
+      {selectPekerjaan === ""
+        ? standarTarifState.listPekerjaan.map((e, i) => _renderTable(e, i))
+        : standarTarifState.listPekerjaan.map(
+            (e, i) =>
+              e.id.toString() === selectPekerjaan.toString() &&
+              _renderTable(e, i)
+          )}
 
       {localStorage.getItem("is_admin") === "true" && (
         <Row
