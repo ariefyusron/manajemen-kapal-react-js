7c752956926e7d16f6280374149a33d9887891b3
Arief Yusron <ariefyusron98@gmail.com>
2020-08-05 01:49:50 +0700

    style topbar

--- src/components/Col/Component.tsx
+++ src/components/Col/Component.tsx
@@ -1,13 +1,21 @@
-import React, { ReactNode, memo } from "react";
+import React, { CSSProperties, ReactNode, SyntheticEvent, memo } from "react";
 
 interface Props {
   children: ReactNode;
   size?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
   className?: string;
+  style?: CSSProperties;
+  onClick?: (e: SyntheticEvent) => void;
 }
 
-const Component = ({ children, size, className }: Props) => (
-  <div className={`col-md-${size} ${className}`}>{children}</div>
+const Component = ({ children, size, className, style, onClick }: Props) => (
+  <div
+    className={`col-md-${size} ${className}`}
+    style={style}
+    onClick={e => onClick && onClick(e)}
+  >
+    {children}
+  </div>
 );
 
 Component.defaultProps = {
--- src/components/Topbar/Component.tsx
+++ src/components/Topbar/Component.tsx
@@ -2,6 +2,8 @@ import React, { memo } from "react";
 import { useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
+import "./styles.scss";
+import { Col, Row } from "..";
 import { handleLogout } from "../../redux/actions";
 
 const Component = () => {
@@ -9,27 +11,34 @@ const Component = () => {
   const dispatch = useDispatch();
 
   return (
-    <nav
-      className="navbar sticky-top navbar-dark bg-dark"
-      style={{ height: 70 }}
-    >
+    <div className="component-topbar sticky-top">
       <div className="container-fluid">
-        <span
-          className="navbar-brand mb-0 h1"
-          onClick={() => history.push("/")}
-        >
-          Home
-        </span>
-        <button
-          className="btn btn-link my-2 my-sm-0"
-          type="button"
-          style={{ color: "white", textDecoration: "underline" }}
-          onClick={() => dispatch(handleLogout(() => history.push("/login")))}
-        >
-          Logout
-        </button>
+        <Row style={{ height: 70 }} className="align-items-center">
+          <Col
+            size={2}
+            className="left"
+            style={{ height: "100%" }}
+            onClick={() => history.push("/")}
+          >
+            <span className="navbar-brand mb-0 h1">Home</span>
+          </Col>
+          <Col size={10}>
+            <Row justifyContent="end">
+              <button
+                className="btn btn-link my-2 my-sm-0"
+                type="button"
+                style={{ color: "white", textDecoration: "underline" }}
+                onClick={() =>
+                  dispatch(handleLogout(() => history.push("/login")))
+                }
+              >
+                Logout
+              </button>
+            </Row>
+          </Col>
+        </Row>
       </div>
-    </nav>
+    </div>
   );
 };
 
