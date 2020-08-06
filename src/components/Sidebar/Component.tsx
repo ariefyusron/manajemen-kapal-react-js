28b57c905892a632cc1a7e18280af900067cf5af
Arief Yusron <ariefyusron98@gmail.com>
2020-08-07 01:20:17 +0700

    add icon

--- src/components/Sidebar/Component.tsx
+++ src/components/Sidebar/Component.tsx
@@ -3,6 +3,7 @@ import { useHistory } from "react-router-dom";
 
 import "./styles.scss";
 import { Container } from "..";
+import { ICONS } from "../../configs";
 
 const Component = () => {
   const history = useHistory();
@@ -10,34 +11,41 @@ const Component = () => {
   const listMenu = [
     {
       title: "Standar tarif",
-      pathname: "/dashboard/standar-tarif"
+      pathname: "/dashboard/standar-tarif",
+      icon: ICONS.standarTarif
     },
     {
       title: "Identitas Kapal",
-      pathname: "/dashboard/identitas-kapal"
+      pathname: "/dashboard/identitas-kapal",
+      icon: ICONS.ship
     },
     {
       title: "Rencana Anggaran Biaya Reparasi Kapal",
-      pathname: "/dashboard/rab-reparasi-kapal"
+      pathname: "/dashboard/rab-reparasi-kapal",
+      icon: ICONS.rab
     },
     {
       title: "Persetujuan RAB",
-      pathname: "/dashboard/persetujuan-rab"
+      pathname: "/dashboard/persetujuan-rab",
+      icon: ICONS.persetujuan
     }
   ];
 
   const listMenuAdmin = [
     {
       title: "User",
-      pathname: "/dashboard/admin/user"
+      pathname: "/dashboard/admin/user",
+      icon: ICONS.users
     },
     {
       title: "Type Kapal",
-      pathname: "/dashboard/admin/kapal"
+      pathname: "/dashboard/admin/kapal",
+      icon: ICONS.typeShip
     },
     {
       title: "Type Survey",
-      pathname: "/dashboard/admin/survey"
+      pathname: "/dashboard/admin/survey",
+      icon: ICONS.survey
     }
   ];
 
@@ -51,6 +59,7 @@ const Component = () => {
               style={{ marginTop: 0 }}
               onClick={() => history.push("/dashboard")}
             >
+              <img src={ICONS.dashboard} alt="dashboard" />
               Dashboard
             </div>
           </li>
@@ -63,11 +72,10 @@ const Component = () => {
           {listMenu.map((item, index) => (
             <li className="nav-item" key={index}>
               <div
-                className={`nav-link item ${
-                  history.location.pathname.includes(item.pathname) ? "h6" : ""
-                }`}
+                className="nav-link item"
                 onClick={() => history.push(item.pathname)}
               >
+                <img src={item.icon} alt={item.title} />
                 {item.title}
               </div>
             </li>
@@ -82,13 +90,10 @@ const Component = () => {
             {listMenuAdmin.map((item, index) => (
               <li className="nav-item" key={index}>
                 <div
-                  className={`nav-link item ${
-                    history.location.pathname.includes(item.pathname)
-                      ? "h6"
-                      : ""
-                  }`}
+                  className="nav-link item"
                   onClick={() => history.push(item.pathname)}
                 >
+                  <img src={item.icon} alt={item.title} />
                   {item.title}
                 </div>
               </li>
--- src/configs/icons.ts
+++ src/configs/icons.ts
@@ -1,5 +1,13 @@
 const icons = {
-  logo: require("../assets/svgs/logo.svg")
+  logo: require("../assets/svgs/logo.svg"),
+  dashboard: require("../assets/svgs/dashboard.svg"),
+  persetujuan: require("../assets/svgs/persetujuan.svg"),
+  ship: require("../assets/svgs/ship.svg"),
+  rab: require("../assets/svgs/rab.svg"),
+  standarTarif: require("../assets/svgs/standarTarif.svg"),
+  users: require("../assets/svgs/users.svg"),
+  typeShip: require("../assets/svgs/typeShip.svg"),
+  survey: require("../assets/svgs/survey.svg")
 };
 
 export default icons;
